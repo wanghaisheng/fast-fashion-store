fca71ff2539ccff26c4d2e041c76fdcac2c51074
Shashank Chittapur <shashank59.chary@gmail.com>
2024-08-24 18:25:53 +0530

    Fix errors

--- src/components/layout/navbar/search.tsx
+++ src/components/layout/navbar/search.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import Form from 'next/form';
 import { useSearchParams } from 'next/navigation';
 
 export default function Search() {
   const searchParams = useSearchParams();
 
   return (
-    <Form action="/search" className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
+    <form action="/search" className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
       <input
         key={searchParams?.get('q')}
         type="text"
@@ -21,7 +20,7 @@ export default function Search() {
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <MagnifyingGlassIcon className="h-4" />
       </div>
-    </Form>
+    </form>
   );
 }
 
